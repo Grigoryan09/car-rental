748dbc5b65997975ae8ef1f9112c46031e81c3fa
Grisha Grigoryan <grisha_grigoryan_0@mail.ru>
2026-01-21 00:36:06 +0400

    add

---
+++ src/main/java/exampleCarRental/listener/MySqlFIxLListener.java
@@ -0,0 +1,19 @@
+package exampleCarRental.listener;
+
+import com.mysql.cj.jdbc.AbandonedConnectionCleanupThread;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+@WebListener
+public class MySqlFIxLListener implements ServletContextListener {
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        try {
+            AbandonedConnectionCleanupThread.checkedShutdown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
